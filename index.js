87b12e26e3a76d2c8b6231c3b9784db79300d110
spdevuk@gmail.com <spdevuk@gmail.com>
2019-08-13 19:15:38 +0100

    feat: add better state and error handling

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
+const updateParserResult = (state, result) => ({
+  ...state,
+  result,
+});
+const updateParserState = (state, index, result) => ({
+  ...state,
+  index,
+  result,
+});
+
+const updateParserError = (state, error) => ({
+  ...state,
+  error,
+  isError: true,
+});
+
 // parser = parserState in  => parserState out
 const str = s => parserState => {
-  const { targetString, index } = parserState;
+  const { targetString, index, isError } = parserState;
+  if (isError) return parserState;
   // success!
-  if (targetString.slice(index).startsWith(s)) {
-    return {
-      ...parserState,
-      result: s,
-      index: index + s.length,
-    };
+
+  const slicedTarget = targetString.slice(index);
+  if (slicedTarget.length === 0)
+    return updateParserState(
+      parserState,
+      `str: Tried to match "${s}" but got Unexpected end of input.`
+    );
+  if (slicedTarget.startsWith(s)) {
+    return updateParserState(parserState, index + s.length, s);
   }
 
-  throw new Error(
-    `Tried to match ${s}, but got ${targetString.slice(index, index + 10)}`
+  return updateParserError(
+    parserState,
+    `Tried to match "${s}", but got "${targetString.slice(index, index + 10)}"`
   );
+};
+
+const sequenceOf = parsers => parserState => {
+  if (parserState.isError) return parserState;
+  const results = [];
+
+  let nextState = parserState;
+
+  // loop through each parser and get next state
+  for (const p of parsers) {
+    // next state is running the parser on the old state
+    nextState = p(nextState);
+    results.push(nextState.result);
+  }
+
+  return updateParserResult(nextState, results);
 };
 
 const run = (parser, targetString) => {
@@ -20,10 +57,12 @@ const run = (parser, targetString) => {
     targetString,
     index: 0,
     result: null,
+    error: null,
+    isError: false,
   };
   return parser(intialState);
 };
 
-const p = str('hello there!');
+const p = sequenceOf([str('hello there!'), str('goodbye there!')]);
 
-console.log(run(p, 'hello there!'));
+console.log(run(p, ''));
