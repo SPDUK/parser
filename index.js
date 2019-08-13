3c2dca1db9a2b683f38f548a7f3a69684a7e5aa1
spdevuk@gmail.com <spdevuk@gmail.com>
2019-08-13 18:19:02 +0100

    feat: very simple str and run parser

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const str = s => {};
+// parser = parserState in  => parserState out
+const str = s => parserState => {
+  const { targetString, index } = parserState;
+  // success!
+  if (targetString.slice(index).startsWith(s)) {
+    return {
+      ...parserState,
+      result: s,
+      index: index + s.length,
+    };
+  }
+
+  throw new Error(
+    `Tried to match ${s}, but got ${targetString.slice(index, index + 10)}`
+  );
+};
+
+const run = (parser, targetString) => {
+  const intialState = {
+    targetString,
+    index: 0,
+    result: null,
+  };
+  return parser(intialState);
+};
+
+const p = str('hello there!');
+
+console.log(run(p, 'hello there!'));
