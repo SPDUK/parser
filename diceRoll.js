cd469eaa26ffe39728ad2e2c299081f2194d4cae
spdevuk@gmail.com <spdevuk@gmail.com>
2019-08-29 20:32:58 +0100

    refactor: move diceRoll to a seperate file

---
+++ diceRoll.js
@@ -0,0 +1,30 @@
+// import parser functions
+
+const stringParser = letters.map(result => ({
+  type: 'string',
+  value: result,
+}));
+
+const numberParser = digits.map(result => ({
+  type: 'number',
+  value: Number(result),
+}));
+
+const dicerollParser = sequenceOf([digits, str('d'), digits]).map(
+  ([n, _, s]) => ({
+    type: 'diceroll',
+    value: [Number(n), Number(s)],
+  })
+);
+
+const parser = sequenceOf([letters, str(':')])
+  .map(results => results[0])
+  .chain(type => {
+    if (type === 'string') return stringParser;
+
+    if (type === 'number') return numberParser;
+
+    return dicerollParser;
+  });
+
+console.log(parser.run('diceroll:2d8'));
